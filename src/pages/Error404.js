1da4acd978d7861de97166f78f05763a5d6f7bb6
Elsa Portilla <elsa.portilla@gmail.com>
2021-06-02 16:38:17 -0500

    Error404 altext added

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,7 @@
 const Error404 = () => {
     const view = `
         <div class="main-error404">
-            <img src="https://i.ibb.co/9rPX9fH/404error.png" alt="alternatetext">
+            <img src="https://i.ibb.co/9rPX9fH/404error.png" alt="Error404">
         </div>
     `;
 
