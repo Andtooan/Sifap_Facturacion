50cc56399917b4c6e3ea4c21b084319cdc0767e2
Elsa Portilla <elsa.portilla@gmail.com>
2021-06-01 22:01:04 -0500

    50% Two Factor autentication layout

--- src/pages/TwoFactor.js
+++ src/pages/TwoFactor.js
@@ -1,8 +1,20 @@
 const TwoFactor = () => {
   let view = `
     <div class="main-twoFactor">
-      <p>two factor</p>
-    </div>
+      <section class="main-twoFactor__card">
+        <img src="https://i.ibb.co/GCfLQ7V/security.png" alt="security shield logo">
+        <h2 class="main-twoFactor__title">Autentica tu cuenta</h2>
+        <p class="main-twoFactor__text">Tu seguridad es nuestra prioridad. Confirme su cuenta ingresando el código de autorización enviado a *******@***</p>
+        <section class="main-twoFactor__input">
+          <input type="text"></input>
+          <input type="text"></input>
+          <input type="text"></input>
+          <input type="text"></input>
+        </section>
+        <button class="main-twoFactor__button" type="button">Enviar</button>
+        <button class="main-twoFactor__button" type="button">Reenviar código</button>
+      </section>
+     </div>
   `
   return view
 };
