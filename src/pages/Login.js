13ad4fc2e067ddf20232a6684779c0a788cc17d5
sebas rivera <bostjanrivera@gmail.com>
2021-06-04 13:22:25 -0500

    responsive fixed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,36 +2,36 @@ const Login = () => {
   const view = `
     <div class="main-login">
       <aside class="aside">
-          <div>
-            <h2 class="titulo">Factura Master</h2>
-            <hr>
-          </div>
-
-          <h2 class="subtitulo">¿Aún no eres cliente?</h2>
-
-          <button class="registro" type="submit"><a href="/#/signup">Registrarme</a></button>
-
-
+        <figure class="logo-login">
+          <img src="https://i.ibb.co/h1hs05Y/factura-master2.png" alt="logo master">
+        </figure>
+        <h2 class="subtitulo">¿Aún no eres cliente?</h2>
+        <button class="registro" type="submit">
+          <a href="/#/signup">Registrarme</a>
+        </button>
       </aside>
 
       <div class="registroContent">
-          <img class="imagenLogin" src="./src/assets/Img/login.svg"/>
-          <h2 class="tituloMobil">Factura Master</h2>
-          <hr class="barraMobil">
-         <div class="formulario">
-            <form>
-              <input type="text" placeholder="Usuario"><br>
+        <img class="imagenLogin" src="./src/assets/Img/login.svg" />
+        <figure class="logo-login">
+          <img src="https://i.ibb.co/h1hs05Y/factura-master2.png" alt="logo master">
+        </figure>
+        <div class="formulario">
+          <form>
+            <input type="text" placeholder="Usuario" /><br />
 
-              <input type="password" placeholder="Password"><br>
+            <input type="password" placeholder="Password" /><br />
 
-              
-              <button class="iniciarSesion" type="submit"><a href="/#/dash">Iniciar Sesión</a></button>
-              <button class="registroMobil" type="submit"><a href="/#/signup">Registrarme</a></button>
-            </form>
-         </div>
+            <button class="iniciarSesion" type="submit">
+              <a href="/#/dash">Iniciar Sesión</a>
+            </button>
+            <button class="registroMobil" type="submit">
+              <a href="/#/signup">Registrarme</a>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-
 
   `;
 
