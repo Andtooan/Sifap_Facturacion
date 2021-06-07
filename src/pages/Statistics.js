9ab00618681d3fdd69a251b19a4671d24dbba5af
sebas rivera <bostjanrivera@gmail.com>
2021-06-07 16:28:30 -0500

    statistic screen finished

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,100 +1,56 @@
+import Aside from '../templates/Aside'
+
 const Statistics = () => {
   let view = `
     <div class="main-statistics">
-      <div class="header">
-        <picture class="aside-logo">
-          <source
-            media="(min-width: 1024px)"
-            srcset="https://i.ibb.co/BZq1pjn/factura-master.png"
-          />
-          <img
-            src="https://i.ibb.co/h1hs05Y/IMG-20210526-183604-2.png"
-            alt="factura master image"
-          />
-        </picture>
-        <figure class="figure">
-          <img src="https://i.ibb.co/NsBQLL2/circle-user.png" alt="user image" />
-        </figure>
-        <nav>
-          <ul class="nav-aside">
-            <li>
-              <img
-                src="https://i.ibb.co/q5tF18r/ecuador-flag-icon.png"
-                alt="ecuador flag"
-              />
-            </li>
-            <li>
-              <a href="/"
-                ><picture>
-                  <source
-                    media="(min-width: 1024px)"
-                    srcset="https://i.ibb.co/zP8J9Lt/white-home-icon.png"
-                  />
-                  <img
-                    src="https://i.ibb.co/RykyPxL/home-icon.png"
-                    alt="home-icon"
-                  /> </picture
-              ></a>
-            </li>
-            <li>
-              <a href=""
-                ><picture>
-                  <source
-                    media="(min-width: 1024px)"
-                    srcset="https://i.ibb.co/5T6djP6/white-notification-icon.png"
-                  />
-                  <img
-                    src="https://i.ibb.co/r0tvszM/notification-icon.png"
-                    alt="notifications icon"
-                  /> </picture
-              ></a>
-            </li>
-            <li>
-              <a href=""
-                ><picture>
-                  <source
-                    media="(min-width: 1024px)"
-                    srcset="https://i.ibb.co/x8kFPX0/white-exit-icon.png"
-                  />
-                  <img
-                    src="https://i.ibb.co/0r5cZCf/exit-icon.png"
-                    alt="log out icon"
-                  /> </picture
-              ></a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      <div class="stadistics">
-        <div class="stadistics__items">
-          <h1>Ingresos</h1>
-          <button class="btn">Ene-Mar 2021</button>
-          <p>Total de Ingresos</p>
-          <h2>$76 666</h2>
-
-          <img src="chart.png" />
+    <aside>
+      ${Aside()}
+    </aside>
+    <div class="statistic-content">
+      <img src="https://i.ibb.co/h1hs05Y/factura-master2.png" alt="Factura master logo">
+      <div class="statistics-card-container">
+        <div class="statistics-card-ingresos">
+          <div class="card-ingresos-header">
+            <h2>Ingresos</h2>
+            <p>Ene - Mar 2021 <span></span></p>
+          </div>
+          <picture>
+            <source media="(min-width: 768px)" srcset="https://i.ibb.co/cXCzJdZ/statistic-desktop.png">
+            <img src="https://i.ibb.co/dDfYfKq/statistic-mobile.png" alt="income statistic">
+          </picture>
+          <div class="card-ingresos-total">
+            <p>Total de Ingresos</p>
+            <h2>$76,685.41</h2>
+            <h3>+ 7.00%</h3>
+          </div>
         </div>
-      </div>
-      <div class="graphic__products">
-        <div class="graphic__products--items">
-          <img src="Widget 3.png" />
-        </div>
-      </div>
-
-      <div class="graphic__costumers">
-        <div class="graphic__costumers--items">
-          <img src="Widget 3.png" />
-        </div>
-      </div>
-
-      <div class="graphic__costumers2">
-        <div class="graphic__costumers2--items">
-          <img src="Widget 3.png" />
+        <div class="circle-card_container">
+          <div class="statistics-card-circle">
+            <h2>Servicios y productos</h2>
+            <img src="https://i.ibb.co/wCzjqhf/circle-statistic.png" alt="Circle statistic">
+            <div class="statistics-card-graphic">
+              <p><span></span> cliente 1</p>
+              <p><span></span> cliente 2</p>
+              <p><span></span> cliente 3</p>
+              <p><span></span> cliente 4</p>
+            </div>
+          </div>
+          <div class="statistics-card-circle">
+            <h2>Clientes</h2>
+            <img src="https://i.ibb.co/wCzjqhf/circle-statistic.png" alt="Circle statistic">
+            <div class="statistics-card-graphic">
+              <p><span></span> cliente 1</p>
+              <p><span></span> cliente 2</p>
+              <p><span></span> cliente 3</p>
+              <p><span></span> cliente 4</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+  </div>
   `;
   return view;
 };
+
 export default Statistics;
